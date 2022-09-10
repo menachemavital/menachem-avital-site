13b89f3dd9621e6ec1480df3d033e0daa3b4899b
menachemavital <avimen@post.bgu.ac.il>
2022-09-10 17:54:49 +0300

    slider works, need to fix gibrish hebrew, need to add welcome popup and create dinamic bookpages

--- static/Script1.js
+++ static/Script1.js
@@ -1,17 +1,82 @@
 // JavaScript source code
-let books = [
-    { "bookName": "קדמת עדן", "author": "ג'ון סטיינבק", "ownerID": "1234", "ownerLocation": "באר שבע", "imgURL": "images/book covers/east of eden.jpeg", "status": "זמין" },
-    { "bookName": "סוס אחד נכנס לבר", "author": "דויד גרוסמםן", "ownerID": "1234", "ownerLocation": "באר שבע", "imgURL": "images/book covers/a horse walks into a bar.jpeg", "status": "לא זמין" }
-]
 
-const p = document.createElement('<div class="card" id="card1"> < div class= "image-content" >div class="card-image"> <img src="../static/images/book covers/a horse walks into a bar.jpeg" class="card-img" id="card1img" /><span class="available"></span></div></div ><div class="card-content"><h4 class"book-title">book1.title</h4><p> location</p><div class="btn"><button class="highlight-button">הזמן</button> <button>פרטים נוספים</button></div></div ></div > ');
 
-function int_slider() {
-    let book1 = JSON.parse(books[0]);
-    let book2 = JSON.parse(books[2]);
-    document.getElementById(card1).appendChild(p);
-   
+let books = new Array();
+
+
+function Book(bookName , author, ownerEmail, coverURL, status, discription) { 
+    this.bookName = bookName;
+    this.author = author;
+    this.wnerEmail = ownerEmail;
+    this.coverURL = coverURL;
+    this.status = status;
+    this.discription = discription;
+    books.push(this);
 }
-function changepic(x) {
-    document.getElementById(x).src = 'a horse walks into a bar.jpeg';
+
+
+
+new Book("html 4", "זוהר עמיהוד", "avimen@post.bgu.ac.il", "../static/images/book covers/html4.jpeg", "זמין", "לא קראתי");
+new Book("ענבי זעם", "ג'ון סטיינבק", "avimen@post.bgu.ac.il", "../static/images/book covers/rage grapes.jpeg","זמין", "אחלה ספר");
+new Book("קדמת עדן", "ג'ון סטיינבק", "avimen@post.bgu.ac.il", "../static/images/book covers/east of eden.jpeg", "זמין", "אחלה ספר");
+new Book("ל עכברים ואנשים", "ג'ון סטיינבק", "avimen@post.bgu.ac.il", "../static/images/book covers/of mice and people.jpeg", "זמין", "אחלה ספר");
+new Book("סוס אחד נכנס לבר", "דויד גרוסמן", "avimen@post.bgu.ac.il", "../static/images/book covers/a horse walks into a bar.jpeg", "זמין", "אחלה ספר");
+new Book("הארי פוטר ומסדר עוף החול", "ג'יי קיי רולינג", "avimen@post.bgu.ac.il", "../static/images/book covers/harry poter 4.jpeg", "זמין", "בהחלט בהחלט");
+new Book("בשורות טובות", "טרי פראצ'ט, ניל גיימן", "avimen@post.bgu.ac.il", "../static/images/book covers/good news.jpeg", "זמין", "משעשע מאוד");
+new Book("תקלה בקצה הגלקסיה", "אתגר קרת", "avimen@post.bgu.ac.il", "../static/images/book covers/error at the edge of the galaxy.jpeg", "זמין", "אחלה ספר");
+new Book("האחים קראמזוב", "פיודור מיכאלוביץ' דוסטוייבסקי", "avimen@post.bgu.ac.il", "../static/images/book covers/the karamazov brothers.jpeg", "זמין", "כבד רצח");
+new Book("רציונליות, הוגנות, אושר", "דניאל כהנמן", "avimen@post.bgu.ac.il", "../static/images/book covers/rationality, fairness, happyness.jpeg", "זמין", "מעניין");
+new Book("כה אמר זרתוסטרה", "פרידריך ניטשה", "avimen@post.bgu.ac.il", "../static/images/book covers/zaratustra.jpeg", "זמין", "ספר לכל אחד ולאף אחד");
+new Book("להדליק מדורה", "ג'ק לונדון", "avimen@post.bgu.ac.il", "../static/images/book covers/to light a fire.jpeg", "זמין", "צאו לטייל באלסקה בחצי שעה של קריאה, קצר ומעולה.");
+
+
+
+
+function firstBook(imgID, status, booktitleId) {
+
+    x = books.shift();
+
+    console.log(x.coverURL);
+
+    document.getElementById(imgID).src = x.coverURL;
+    document.getElementById(status).innerHTML = x.status;
+    document.getElementById(booktitleId).innerHTML = x.bookName;
+    books.push(x);
 }
+
+
+function lastBook(imgID, status, booktitleId) {
+
+    x = books.pop();
+
+    console.log(x.coverURL);
+
+    document.getElementById(imgID).src = x.coverURL;
+    document.getElementById(status).innerHTML = x.status;
+    document.getElementById(booktitleId).innerHTML = x.bookName;
+    books.unshift(x);
+}
+
+
+function rightOrder() {
+
+    firstBook('card1img', 'card1-available', 'card1-title');
+    firstBook('card2img', 'card2-available', 'card2-title');
+    firstBook('card3img', 'card3-available', 'card3-title');
+    firstBook('card4img', 'card4-available', 'card4-title');
+}
+
+function leftOrder() {
+
+    lastBook('card1img', 'card1-available', 'card1-title');
+    lastBook('card2img', 'card2-available', 'card2-title');
+    lastBook('card3img', 'card3-available', 'card3-title');
+    lastBook('card4img', 'card4-available', 'card4-title');
+}
+
+
+function showStatus(imgid, statusID) {
+    document.getElementById(statusID).innerHTML
+}
+
+
